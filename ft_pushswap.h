0aa1d63bec2fc25676cd0476a4a8af0cdd86ef8a
Quentin Brillais <qbrillai@c1r4p7.42nice.fr>
2021-07-20 14:34:58 +0200

    new commit

---
+++ ft_pushswap.h
@@ -0,0 +1,8 @@
+#ifndef FT_PUSHSWAP_H
+# define FT_PUSHSWAP_H
+
+# include "libft/libft.h"
+
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,15 @@
+#include "ft_pushswap.h"
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int	*a;
+
+	i = 0;
+	while (av[i])
+	{
+		i++;
+	}
+	a = malloc(sizeof(int) * i);
+	
+}
